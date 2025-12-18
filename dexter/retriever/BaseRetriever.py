c5d849ea93bebe048e24bcac0daf123248f7b70a
bbucksch <117900119+bbucksch@users.noreply.github.com>
2025-12-18 01:31:30 +0100

    Add files via upload

---
+++ dexter/retriever/BaseRetriever.py
@@ -0,0 +1,25 @@
+'''Base class for a retriever'''
+class BaseRetriver:
+    def __init__(self) -> None:
+        pass
+
+    def encode_queries(self)->None:
+        pass
+
+    def encode_context(self)->None:
+        pass
+
+    def train(self)->None:
+        pass
+
+    def retrieve(self)->None:
+        pass
+
+'''Retriever Factory to get retriever class given alias name'''
+class RetrieverFactory():
+    def get_retreiver(self,retriever_name:str,config_path:str):
+        if retriever_name.lower() == 'adore':
+            from .dense.ADORE import ADORERetriever
+            return ADORERetriever
+        return None
+
