68be99f905ec51a61a7997449a26c0526f52bb56
bbucksch <117900119+bbucksch@users.noreply.github.com>
2026-01-17 12:43:04 +0100

    Add files via upload

---
+++ dexter/config/constants.py
@@ -0,0 +1,23 @@
+class Split:
+    TEST = "test"
+    TRAIN = "train"
+    DEV = "dev"
+    PRED = "predict"
+
+class Dataset:
+    AMBIGQA = "ambignq"
+    WIKIMULTIHOPQA = "wikimultihopqa"
+    FINQA = "finqa"
+    TATQA = "tatqa"
+    MUSIQUEQA="musiqueqa"
+    OTTQA = "ottqa"
+    StrategyQA = "strategyqa"
+    MY_DATASET = "my-dataset"
+
+class Separators:
+    TABLE_ROW_SEP = '\n'
+    TABLE_COL_SEP = '|'
+
+class DataTypes:
+    TABLE = "table"
+    TEXT = "text"
